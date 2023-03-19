69fde665b72a91ae799ebfa2567eb97e9821fba1
Matthew Ramamonjisoa <matt30335@gmail.com>
2023-03-19 01:16:01 -0400

    commit before changing to double

--- main.cpp
+++ main.cpp
@@ -13,16 +13,16 @@
 int main(){
     Transform SpherePos = Translate(Vector3f(0, 0.5, -5));
     Transform SpherePosInv = Inv(SpherePos);
-    Transform SpherePos2 = Translate(Vector3f(0, -10, -5));
+    Transform SpherePos2 = Translate(Vector3f(0, -100, -5));
     Transform SpherePosInv2 = Inv(SpherePos2);
     Transform Identity;
-    const Point2i resolution{256, 256};
+    const Point2i resolution{512, 512};
 
 
 
 
     std::shared_ptr<Shape> sphere1_shape = std::make_shared<Sphere>(&SpherePos, &SpherePosInv, 1.0);
-    std::shared_ptr<Shape> sphere2_shape = std::make_shared<Sphere>(&SpherePos2, &SpherePosInv2, 9.0);
+    std::shared_ptr<Shape> sphere2_shape = std::make_shared<Sphere>(&SpherePos2, &SpherePosInv2, 99.0);
     std::shared_ptr<Primitive> sphere1_prim = std::make_shared<GeoPrimitive>(sphere1_shape);
     std::shared_ptr<Primitive> sphere2_prim = std::make_shared<GeoPrimitive>(sphere2_shape);
 
@@ -32,7 +32,7 @@ int main(){
 
     std::shared_ptr<Film> film = std::make_shared<Film>();
     std::shared_ptr<Camera> camera = std::make_shared<PerspectiveCamera>(&Identity, resolution, 250);
-    std::shared_ptr<PixelSampler> sampler = std::make_shared<StratifiedSampler>(1024);
+    std::shared_ptr<PixelSampler> sampler = std::make_shared<StratifiedSampler>(100);
     std::cout << sampler->samplesPerPixel << std::endl;
     
     LambertIntegrator pog{film, camera, sampler};
